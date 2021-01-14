93ee819647f814b1cfe122812df53066a11e882d
rishabhlingam <74484036+rishabhlingam@users.noreply.github.com>
2021-01-14 16:42:23 +0530

    Removed commented code 6
    Removed unnecessary lines of code that were commented out.
    file path : mvisc/src/com/zooracle/view/swing/PhotoList.java

--- src/com/zooracle/view/swing/PhotoList.java
+++ src/com/zooracle/view/swing/PhotoList.java
@@ -20,7 +20,6 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
-//import main.ImageEditor;
 
 public class PhotoList extends JPanel
 {
@@ -38,7 +37,6 @@ public class PhotoList extends JPanel
 
 	private DefaultTableModel model;
 
-//	private String columnNames[] = new String[] { Locale.labelAlias, Locale.labelId, Locale.labelPhotoName };
 	private String columnNames[] = new String[] { Locale.labelPhotoName };
 	
 	private ArrayList<Integer> itemEdited = new ArrayList<Integer>();
@@ -50,7 +48,6 @@ public class PhotoList extends JPanel
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		this.zooracleContentPanel = zooracleContentPanel;
 		// Set the frame characteristics
-//		setSize(150, 600);
 		setBackground(Color.gray);
 
 		// Create a panel to hold all other components
@@ -59,22 +56,13 @@ public class PhotoList extends JPanel
 
 		this.add(topPanel);
 
-
-		// Create some data
-//		String dataValues[][] = { { "12", "234", "67" }, { "-123", "43", "853" }, { "93", "89.2", "109" }, { "279", "9033", "3092" } };
-
 		// Create a new table instance
 		table = new JTable(null, columnNames);
-//		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-//		table.setMinimumSize(new Dimension(150, 600));
-//		table.setPreferredSize(new Dimension(150, 600));
 
 		// table.setD
 		selectionModel = new DefaultListSelectionModel();
 		
 		model = new DefaultTableModel();
-		
 
 		// table.setC
 		selectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -98,38 +86,23 @@ public class PhotoList extends JPanel
 					{
 						((ImportView)(PhotoList.this.zooracleContentPanel)).setCurrentPhoto(selectionIndex);
 					}
-//					if (selectionIndex > 2)
-//						PhotoList.this.maximize(false);
 				}
 			}
 		});
-		// table.setDefaultRenderer(Object.class, new EditedCellRenderer(this));
+		
 		table.setSelectionModel(selectionModel);
 		table.setDefaultRenderer(String.class, new BoardTableCellRenderer());
 		table.setDefaultRenderer(Object.class, new BoardTableCellRenderer());
 		
 		table.setModel(model);
-//		table.setMaximumSize(new Dimension(100, 300));
 		
 
 		// Add the table to a scrolling pane
 		scrollPane = new JScrollPane();
-//		scrollPane.add(table.getTableHeader());
-//		scrollPane.add(table);
-//		topPanel.add(scrollPane);
 		
 		topPanel.add(table.getTableHeader());
 		topPanel.add(new JScrollPane(table));
 	}
-	//
-	// // Main entry point for this example
-	// public static void main(String args[])
-	// {
-	// // Create an instance of the test application
-	// PhotoList mainFrame = new PhotoList();
-	//
-	// mainFrame.setVisible(true);
-	// }
 
 	public void maximize(boolean b)
 	{
@@ -147,7 +120,6 @@ public class PhotoList extends JPanel
 			topPanel.setPreferredSize(new Dimension(180, 600));
 			topPanel.setMaximumSize(new Dimension(190, 600));
 		}
-		// System.out.println("repaintin");
 		this.revalidate();
 		this.repaint();
 	}
@@ -168,18 +140,6 @@ public class PhotoList extends JPanel
 		scrollPane.revalidate();
 		this.revalidate();
 		this.repaint();
-//		for (int i = model.getMaxSelectionIndex(); i > 0; i--)
-//		{
-//		if (model.getRowCount() > 0) {
-//		    for (int i = myTableModel.getRowCount() - 1; i > -1; i--) {
-//		        myTableModel.removeRow(i);
-//		    }
-//		}
-		
-//		model.removeIndexInterval(index0, index1);
-//		}
-
-		// model.r
 	}
 
 	class BoardTableCellRenderer extends DefaultTableCellRenderer
@@ -198,7 +158,6 @@ public class PhotoList extends JPanel
 				s = valueAt.toString();
 			}
 
-//			if (s.contains("0"))
 			if (isSelected)
 			{
 				c.setForeground(GUISettings.itemSelectedForegroundColor);
