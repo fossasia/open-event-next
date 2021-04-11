a56b18772430a33df10b47d900754e27a77216dd
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-04-11 20:44:45 +0530

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core/styles'
-import theme from '../src/theme'
-
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
