8a428d080bde2cb4b74899829342eb9ca332b4b7
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-04-11 16:51:23 +0530

    chore: setup material-ui for the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import React from 'react'
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
