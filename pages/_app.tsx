9c23d578e3a3bac4c645925aae8d5e6d8c30a7e2
Pavnesh Chaturvedi <56963647+pc-beast@users.noreply.github.com>
2021-04-12 22:37:46 +0530

    chore: setup material-ui for the project (#12)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
+import React from 'react'
 import { AppProps } from 'next/app'
+import { StylesProvider, ThemeProvider } from '@material-ui/core/styles'
 import '../styles/globals.css'
+import theme from '../src/theme'
+import { CssBaseline } from '@material-ui/core'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+  return (
+    <ThemeProvider theme={theme}>
+      <StylesProvider injectFirst>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </StylesProvider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/_document.tsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+import { ServerStyleSheets } from '@material-ui/core/styles'
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* PWA primary color */}
+          <link
+            rel="preload"
+            href="/fonts/roboto-v27-latin-300.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/roboto-v27-latin-regular.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/roboto-v27-latin-500.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/roboto-v27-latin-700.woff2"
+            as="font"
+            crossOrigin=""
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+// `getInitialProps` belongs to `_document` (instead of `_app`),
+// it's compatible with server-side generation (SSG).
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  }
+}
