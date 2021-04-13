d928284a148430099b1a19b5f319dda0cdaee222
Dev Valecha <devvalecha07@gmail.com>
2021-04-14 00:30:46 +0530

    chore: Loader-setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Router from 'next/router'
+import Link from 'next/link'
+import Head from 'next/head'
+import NProgress from 'nprogress'
 import { AppProps } from 'next/app'
 import { StylesProvider, ThemeProvider } from '@material-ui/core/styles'
 import '../styles/globals.css'
@@ -7,7 +11,11 @@ import { CssBaseline } from '@material-ui/core'
 import { init } from '../utils/sentry'
 
 init()
-
+Router.events.on('routeChangeStart', () => {
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 function MyApp({
   Component,
   pageProps,
@@ -21,12 +29,29 @@ function MyApp({
     }
   }, [])
   return (
-    <ThemeProvider theme={theme}>
-      <StylesProvider injectFirst>
-        <CssBaseline />
-        <Component {...pageProps} err={err} />
-      </StylesProvider>
-    </ThemeProvider>
+    <div>
+      <ThemeProvider theme={theme}>
+        <StylesProvider injectFirst>
+          <CssBaseline />
+          <Component {...pageProps} err={err} />
+        </StylesProvider>
+      </ThemeProvider>
+      <Head>
+        {/* Import CSS for nprogress */}
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
+      <nav>
+        <style jsx>{`
+          a {
+            margin: 0 10px 0 0;
+          }
+        `}</style>
+        <Link href="/">
+          <a></a>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </div>
   )
 }
 
