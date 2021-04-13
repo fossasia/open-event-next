b2309fc67268be1e3c9a65e86ecf8f6c8bf586c7
Dev Valecha <devvalecha07@gmail.com>
2021-04-13 22:02:40 +0530

    chore: loader bar added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import Router from 'next/router'
+import Link from 'next/link'
+import Head from 'next/head'
+import NProgress from 'nprogress'
 import { AppProps } from 'next/app'
 import { StylesProvider, ThemeProvider } from '@material-ui/core/styles'
 import '../styles/globals.css'
 import theme from '../src/theme'
 import { CssBaseline } from '@material-ui/core'
+Router.events.on('routeChangeStart', () => {
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        {/* Import CSS for nprogress */}
+        <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+      </Head>
+      <nav>
+        <style jsx>{`
+          a {
+            margin: 0 10px 0 0;
+          }
+        `}</style>
+        <Link href="/">
+          <a></a>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  )
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side')
