14ee08cbc10cbd837fd8964713ba931f95d466bb
Areeb Jamal <jamal.areeb@gmail.com>
2021-04-17 04:02:04 +0530

    f

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -9,7 +9,7 @@ i18n.loadLocaleData('hi', { plurals: hi })
  * This function isn't part of the LinguiJS library because there're
  * many ways how to load messages — from REST API, from file, from cache, etc.
  */
-export async function activate(locale: string) {
+export async function activate(locale: string): Promise<void> {
   const { messages } = await import(`../locale/${locale}/messages.js`)
   i18n.load(locale, messages)
   i18n.activate(locale)
