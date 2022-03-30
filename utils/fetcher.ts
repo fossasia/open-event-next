5501f7daa1bd843f9312808d56906b51da966672
Shubham Upreti <shubham.upreti@gmail.com>
2022-03-30 12:27:36 +0530

    fix again

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -2,17 +2,13 @@ import { instance } from '../src/ky/instance'
 
 interface Props {
   url: string
-  method?: string
 }
 
 export default async function fetcher({
   url,
-  method = 'GET',
 }: Props): Promise<[data: ServerProp | null, err: any]> {
   try {
-    const data: ServerProp = await instance(url, {
-      method: method,
-    }).then((res) => res.json())
+    const data: ServerProp = await instance.get(url).json()
 
     return [data, null]
   } catch (err) {
